cf6be2910f94f869ba462bd0099c8f9837b2b603
MD Ripon Islam <islam876ripon@gmail.com>
2023-02-24 01:38:14 +0600

    basic schema added

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -2,10 +2,38 @@ const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
 
+type User {
+	_id: ID!
+	username: String!
+	password: String
+	blogs: [Blog!]!
+	comments: [Comment!]!
+}
+
+type Blog {
+	_id: ID!
+	title: String!
+	content: String!
+	author: User!
+	comments: [Comment!]!
+	creation_date: String!
+}
+
+type Comment {
+	_id: ID!
+	comment: String!
+	blog: Blog!
+	author: User!
+	creation_date: String!
+} 
+
+type  RootQuery {
+	users: [User!]!
+}
 
 
-
-
-
+schema {
+	query : RootQuery 
+}
 
 `);
