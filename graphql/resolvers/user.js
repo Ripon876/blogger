bce6b3f4b002ff77ed72ee3ff3cfe45aefffc327
MD Ripon Islam <islam876ripon@gmail.com>
2023-02-24 07:55:08 +0600

    basic mutaion added

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,12 +1,26 @@
+const bcrypt = require("bcrypt");
 const User = require("../../models/user");
 
 module.exports = {
-	users: async (args) => {
+	users: async () => {
 		try {
 			users = await User.find({});
 			return users;
 		} catch (err) {
 			console.log(err);
+		}
+	},
+	createUser: async (args) => {
+		try {
+			let hashedPWD = await bcrypt.hash(args.input.password, 10);
+			let user = await new User({
+				username: args.input.username,
+				password: hashedPWD,
+			});
+			await user.save();
+			return user;
+		} catch (err) {
+			throw err;
 		}
 	},
 };
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -2,38 +2,48 @@ const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
 
-type User {
-	_id: ID!
-	username: String!
-	password: String
-	blogs: [Blog!]!
-	comments: [Comment!]!
-}
+		type User {
+			_id: ID!
+			username: String!
+			password: String
+			blogs: [Blog!]!
+			comments: [Comment!]!
+		}
 
-type Blog {
-	_id: ID!
-	title: String!
-	content: String!
-	author: User!
-	comments: [Comment!]!
-	creation_date: String!
-}
+		type Blog {
+			_id: ID!
+			title: String!
+			content: String!
+			author: User!
+			comments: [Comment!]!
+			creation_date: String!
+		}
 
-type Comment {
-	_id: ID!
-	comment: String!
-	blog: Blog!
-	author: User!
-	creation_date: String!
-} 
+		type Comment {
+			_id: ID!
+			comment: String!
+			blog: Blog!
+			author: User!
+			creation_date: String!
+		} 
 
-type  RootQuery {
-	users: [User!]!
-}
+		type  RootQuery {
+			users: [User!]!
+		}
 
 
-schema {
-	query : RootQuery 
-}
+		input UserInput {
+			username: String!
+			password: String!
+		}
+
+		type RootMutation {
+			createUser(input: UserInput) : User!
+		}
+
+		schema {
+			query : RootQuery 
+			mutation: RootMutation
+		}
 
 `);
